698eda138da9710dc03f87649c6feef66c967579
Temirlan Aitzhanov <tem.aitzhanov@yandex.kz>
2020-08-10 03:32:37 +0600

    Готовая версия

---
+++ script/script.js
@@ -0,0 +1,42 @@
+let popup = document.querySelector('.popup');
+let popupOpenButton = document.querySelector('.profile__pic-edit');
+let popupCloseButton = popup.querySelector('.popup__close');
+let popupSaveButton = document.querySelector('.popup__submit');
+let page = document.querySelector('.page');
+let form = popup.querySelector('.popup__form');
+let nameInput = form.querySelector('.popup__name');
+let jobInput = form.querySelector('.popup__workplace');
+let name = document.querySelector('.profile__name');
+let profession = document.querySelector('.profile__profession');
+//Функция меняющая класс по нажатию
+let popupToggle = function(event){
+  event.preventDefault();
+  popup.classList.toggle('popup__openede');
+}
+//События на кнопки закрытия и открытие Pop-Up
+popupOpenButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
+//Функция по закрытию Pop-up нажатием в любую область
+let closePopup = function(){
+  if(event.target !== event.currentTarget){
+    return;
+  }
+  popupToggle(event);
+}
+//Событие
+popup.addEventListener('click', closePopup);
+//Функция считывает значение value
+function ReadValue(evt){
+  evt.preventDefault();
+  nameInput.value = document.getElementsByName('name')['0'].textContent;
+  jobInput.value = document.getElementsByName('job')['0'].textContent;
+}
+//Функция записывает значение value
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    name.textContent = nameInput.value;
+    profession.textContent = jobInput.value;
+}
+//События формы и кнопки submit
+form.addEventListener('click', formSubmitHandler);
+popupSaveButton.addEventListener('click', closePopup);
