729fb95c5ceb910cd0f8f352e52feaab4be99874
Temirlan Aitzhanov <tem.aitzhanov@yandex.kz>
2020-10-11 13:29:37 +0600

    Hot Fix with index.html & js & blocks

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 let blockPopup = document.querySelector('.popup');
 let openBlockPopup = document.querySelector('.profile__pic-edit');
 let closeBlockPopup = blockPopup.querySelector('.popup__close');
-let saveBlockPopup = blockPopup.querySelector('.popup__submit');
 let nameFromPage = document.querySelector('.profile__name');
 let jobFromPage = document.querySelector('.profile__profession');
 let nameFromForm = document.querySelector('.popup__input_name');
@@ -10,6 +9,9 @@ function togglePopup(){
   blockPopup.classList.toggle('popup_opened');
   nameFromForm.value = nameFromPage.textContent;
   jobFromForm.value = jobFromPage.textContent;
+}
+function closePopup(){
+  blockPopup.classList.toggle('popup_opened');
 }
 function savePopup (event){
   event.preventDefault();
